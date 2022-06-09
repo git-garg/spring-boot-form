f24cb46e0d87fbb6ccdf41e75fe2f869ca028e11
garg010222 <garg010222@sri.gob.ec>
2022-06-09 16:08:16 -0500

    se restablece archivos

---
+++ src/test/java/ec/com/empresa/app/SpringBootFormApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.empresa.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
